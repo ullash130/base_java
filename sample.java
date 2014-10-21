1fdfbd755de9c6c7005c367a0d9dd9c81f09e56a
Khandaker Abdul Muntashir <ullash130@users.noreply.github.com>
2014-10-21 22:06:56 +0600

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,6 +3,9 @@ package newPack;
 public class one{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		for(int i = 0; i<11; i++){
+			System.out.println("Hello World! "+i+1);
+		}
 	}
 
 }
